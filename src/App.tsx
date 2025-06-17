85ae1212fa406b783181b4f54c54fe0b78ac71cd
Dario George <edu.dariogeorge21@gmail.com>
2025-06-17 23:31:04 +0530

    toggle button for theme

--- src/App.tsx
+++ src/App.tsx
@@ -19,10 +19,12 @@ function App() {
 
   return (
     <>
-      <div className="header">Calculator</div>
-      <div className="right">
-          <button className="button" onClick={toggleTheme}>Switch to {isDark ? 'Light' : 'Dark'} Mode</button>
-      </div>
+     <div className="header"> <div className="header">CALCULATOR</div>
+      <button className="button" onClick={toggleTheme}>{isDark ?<img src="/sun.svg" alt="Backspace" style={{ width: 24, height: 24 }} />
+ :             <img src="/moon.svg" alt="Backspace" style={{ width: 24, height: 24 }} />
+}</button>
+    </div>
+      
       <div className="container">
         <div className="result">Result: </div>
         <div className="result">Question: </div>
@@ -46,10 +48,7 @@ function App() {
           <button className="box">00</button>
           <button className="box">=</button>
           <button className="box">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" className="bi bi-backspace" viewBox="0 0 16 16">
-              <path d="M5.83 5.146a.5.5 0 0 0 0 .708L7.975 8l-2.147 2.146a.5.5 0 0 0 .707.708l2.147-2.147 2.146 2.147a.5.5 0 0 0 .707-.708L9.39 8l2.146-2.146a.5.5 0 0 0-.707-.708L8.683 7.293 6.536 5.146a.5.5 0 0 0-.707 0z"/>
-              <path d="M13.683 1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-7.08a2 2 0 0 1-1.519-.698L.241 8.65a1 1 0 0 1 0-1.302L5.084 1.7A2 2 0 0 1 6.603 1zm-7.08 1a1 1 0 0 0-.76.35L1 8l4.844 5.65a1 1 0 0 0 .759.35h7.08a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1z"/>
-            </svg>
+            <img src="/backspace.svg" alt="Backspace" style={{ width: 24, height: 24 }} />
           </button>
           <button className="box">AC</button>
         </div>
